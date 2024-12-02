86f5f8d844a7fd1c110227ba7432f5e0c3ef01eb
Nelson-hash <nlsn.remy@gmail.com>
2024-12-02 11:03:10 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: './', // This ensures assets are loaded correctly on GitHub Pages
+  publicDir: 'public',
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
