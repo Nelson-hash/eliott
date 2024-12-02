39d6bd0e386432cefb07ef6f051e20083157fbd2
Nelson-hash <nlsn.remy@gmail.com>
2024-12-02 11:33:51 +0100

    Add files via upload

--- data/projects.ts
+++ data/projects.ts
@@ -1,69 +1,75 @@
-import { Project } from '../types/project';
+export interface Project {
+  id: string;
+  title: string;
+  coverImage: string;
+  images: string[];
+  description: string;
+}
 
 export const projects: Project[] = [
   {
     id: 'chariot-de-course',
     title: 'CHARIOT DE COURSE',
-    coverImage: 'chariot-de-course/cover.jpg',
+    coverImage: '/images/chariot-de-course/cover.jpg',
     images: [
-      'chariot-de-course/01.jpg',
-      'chariot-de-course/02.jpg',
-      'chariot-de-course/03.jpg'
+      '/images/chariot-de-course/01.jpg',
+      '/images/chariot-de-course/02.jpg',
+      '/images/chariot-de-course/03.jpg'
     ],
     description: 'Street photography capturing the essence of urban life through portraiture'
   },
   {
     id: 'rideau-message',
     title: 'RIDEAU MESSAGE',
-    coverImage: 'rideau-message/cover.jpg',
+    coverImage: '/images/rideau-message/cover.jpg',
     images: [
-      'rideau-message/01.jpg',
-      'rideau-message/02.jpg',
-      'rideau-message/03.jpg'
+      '/images/rideau-message/01.jpg',
+      '/images/rideau-message/02.jpg',
+      '/images/rideau-message/03.jpg'
     ],
     description: 'Vertical exploration of modern architectural forms'
   },
   {
     id: 'puzzle-marrant',
     title: 'PUZZLE MARRANT',
-    coverImage: 'puzzle-marrant/cover.jpg',
+    coverImage: '/images/puzzle-marrant/cover.jpg',
     images: [
-      'puzzle-marrant/01.jpg',
-      'puzzle-marrant/02.jpg',
-      'puzzle-marrant/03.jpg'
+      '/images/puzzle-marrant/01.jpg',
+      '/images/puzzle-marrant/02.jpg',
+      '/images/puzzle-marrant/03.jpg'
     ],
     description: 'A study of natural light and dramatic shadows in vertical compositions'
   },
   {
     id: 'tipi',
     title: 'TIPI',
-    coverImage: 'tipi/cover.jpg',
+    coverImage: '/images/tipi/cover.jpg',
     images: [
-      'tipi/01.jpg',
-      'tipi/02.jpg',
-      'tipi/03.jpg'
+      '/images/tipi/01.jpg',
+      '/images/tipi/02.jpg',
+      '/images/tipi/03.jpg'
     ],
     description: 'Exploring isolation and contemplation in urban environments'
   },
   {
     id: 'maison-beton',
     title: 'MAISON BETON',
-    coverImage: 'maison-beton/cover.jpg',
+    coverImage: '/images/maison-beton/cover.jpg',
     images: [
-      'maison-beton/01.jpg',
-      'maison-beton/02.jpg',
-      'maison-beton/03.jpg'
+      '/images/maison-beton/01.jpg',
+      '/images/maison-beton/02.jpg',
+      '/images/maison-beton/03.jpg'
     ],
     description: 'Abstract architectural details and geometric patterns'
   },
   {
     id: 'message-tissus',
     title: 'MESSAGE TISSUS',
-    coverImage: 'message-tissus/cover.jpg',
+    coverImage: '/images/message-tissus/cover.jpg',
     images: [
-      'message-tissus/01.jpg',
-      'message-tissus/02.jpg',
-      'message-tissus/03.jpg'
+      '/images/message-tissus/01.jpg',
+      '/images/message-tissus/02.jpg',
+      '/images/message-tissus/03.jpg'
     ],
     description: 'Exploring textile patterns and woven messages in urban spaces'
   }
