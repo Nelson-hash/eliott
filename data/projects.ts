ad51ab3d9bf5ad2e6760f496b5fc5d5d564e5bd7
Nelson-hash <nlsn.remy@gmail.com>
2025-03-27 11:42:59 +0100

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -12,53 +12,20 @@ export const projects: Project[] = [
     title: 'CHARIOT DE COURSE',
     coverImage: '/images/chariot-de-course/cover.jpg',
     images: [
+      '/images/chariot-de-course/cover.jpg',
       '/images/chariot-de-course/01.jpg',
-      '/images/chariot-de-course/02.jpg',
-      '/images/chariot-de-course/03.jpg'
+      '/images/chariot-de-course/02.jpg'
     ],
     description: 'Street photography capturing the essence of urban life through portraiture'
-  },
-  {
-    id: 'rideau-message',
-    title: 'RIDEAU MESSAGE',
-    coverImage: '/images/rideau-message/cover.jpg',
-    images: [
-      '/images/rideau-message/01.jpg',
-      '/images/rideau-message/02.jpg',
-      '/images/rideau-message/03.jpg'
-    ],
-    description: 'Vertical exploration of modern architectural forms'
-  },
-  {
-    id: 'puzzle-marrant',
-    title: 'PUZZLE MARRANT',
-    coverImage: '/images/puzzle-marrant/cover.jpg',
-    images: [
-      '/images/puzzle-marrant/01.jpg',
-      '/images/puzzle-marrant/02.jpg',
-      '/images/puzzle-marrant/03.jpg'
-    ],
-    description: 'A study of natural light and dramatic shadows in vertical compositions'
-  },
-  {
-    id: 'tipi',
-    title: 'TIPI',
-    coverImage: '/images/tipi/cover.jpg',
-    images: [
-      '/images/tipi/01.jpg',
-      '/images/tipi/02.jpg',
-      '/images/tipi/03.jpg'
-    ],
-    description: 'Exploring isolation and contemplation in urban environments'
   },
   {
     id: 'maison-beton',
     title: 'MAISON BETON',
     coverImage: '/images/maison-beton/cover.jpg',
     images: [
+      '/images/maison-beton/cover.jpg',
       '/images/maison-beton/01.jpg',
-      '/images/maison-beton/02.jpg',
-      '/images/maison-beton/03.jpg'
+      '/images/maison-beton/02.jpg'
     ],
     description: 'Abstract architectural details and geometric patterns'
   },
@@ -67,10 +34,43 @@ export const projects: Project[] = [
     title: 'MESSAGE TISSUS',
     coverImage: '/images/message-tissus/cover.jpg',
     images: [
+      '/images/message-tissus/cover.jpg',
       '/images/message-tissus/01.jpg',
-      '/images/message-tissus/02.jpg',
-      '/images/message-tissus/03.jpg'
+      '/images/message-tissus/02.jpg'
     ],
     description: 'Exploring textile patterns and woven messages in urban spaces'
+  },
+  {
+    id: 'puzzle-marrant',
+    title: 'PUZZLE MARRANT',
+    coverImage: '/images/puzzle-marrant/cover.jpg',
+    images: [
+      '/images/puzzle-marrant/cover.jpg',
+      '/images/puzzle-marrant/01.jpg',
+      '/images/puzzle-marrant/02.jpg'
+    ],
+    description: 'A study of natural light and dramatic shadows in vertical compositions'
+  },
+  {
+    id: 'rideau-message',
+    title: 'RIDEAU MESSAGE',
+    coverImage: '/images/rideau-message/cover.jpg',
+    images: [
+      '/images/rideau-message/cover.jpg',
+      '/images/rideau-message/01.jpg',
+      '/images/rideau-message/02.jpg'
+    ],
+    description: 'Vertical exploration of modern architectural forms'
+  },
+  {
+    id: 'tipi',
+    title: 'TIPI',
+    coverImage: '/images/tipi/cover.jpg',
+    images: [
+      '/images/tipi/cover.jpg',
+      '/images/tipi/01.jpg',
+      '/images/tipi/02.jpg'
+    ],
+    description: 'Exploring isolation and contemplation in urban environments'
   }
 ];
