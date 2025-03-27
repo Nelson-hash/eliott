0ac24d5be8f9d476b4bf7fdc0273e9fa379d0ad8
Nelson-hash <nlsn.remy@gmail.com>
2025-03-27 14:52:52 +0100

    Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -7,7 +7,7 @@ export function AboutPage() {
         <h1 className="text-3xl font-light text-gray-900 mb-8">À propos</h1>
         <div className="prose text-gray-600">
           <p className="mb-6">
-            JE MAPPELLE ELIOTT ET DANS LA VIE JE BRICOLE.
+            JE MAPPELLE ELIOTT ET DANS LA VIE JE BRICOLE TOUTES SORTES DE CHOSES.
           </p>
           <p>
             MON INSTA : @ELIOTTYISDROOPY
