3dd5cffbac111599bfbc82b9b6557e412f2f474d
Nelson-hash <nlsn.remy@gmail.com>
2025-07-11 10:40:52 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,12 @@ export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50">
       <nav className="max-w-7xl mx-auto px-4 py-6 flex justify-between items-center">
-        <Link to="/" className="text-gray-900 font-light text-xl">
+        <Link to="/home" className="text-gray-900 font-light text-xl">
           Eliott Goyeau
         </Link>
         <div className="space-x-8">
           <Link to="/vrac" className="px-3 py-1 border border-black hover:bg-black hover:text-white transition-colors">
             Vrac
-          </Link>
-          <Link to="/about" className="px-3 py-1 border border-black hover:bg-black hover:text-white transition-colors">
-            À propos
           </Link>
         </div>
       </nav>
